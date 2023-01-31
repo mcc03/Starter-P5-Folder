f69ca072cff3fe28dda8aeaa9689f82799be5002
N00212147 <N00212147@gmail.com>
2023-01-31 16:23:00 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,63 @@
+let numBlocks = 9;
+let chartWidth = 400;
+let chartHeight = 400;
+let marginLeft = 10;
+let marginRight = 10;
+let blockGap = 20;
+let blockWidth = (chartWidth - (marginLeft + marginRight) - ((numBlocks - 1)* blockGap))/numBlocks;
+let screenWidth = 500;
+let screenHeight = 500;
+let firstBlockxPos = ((screenWidth - chartWidth)/2)+ marginLeft;
+let masterGap = blockWidth+blockGap;
+
+console.log(blockWidth)
+
+
+// called once
+function setup(){
+    createCanvas(screenWidth,screenHeight);
+    noLoop();
+}
+
+// called 60times per second
+function draw(){
+    background(200);
+    fill(0);
+    for(let x=0; x < numBlocks; x++){
+        push();
+        translate(firstBlockxPos + (x*masterGap), chartHeight);
+        rect(0,0,blockWidth,random(-50,-400));
+        pop();
+    }
+  
+    // noFill();
+    // line(50,50,50,450)
+    // line(450,450,50,450)
+
+    // stroke(0);
+    // strokeWeight(4);
+    // fill(0,255,0);
+
+    // // bars
+    // fill(0,0,255)
+    // rect(100,230,50,220);
+    // fill(0,0,200)
+    // rect(200,200,50,250);
+    // fill(0,0,150)
+    // rect(300,170,50,280);
+    // fill(0,0,100)
+    // rect(400,150,50,300);
+}
+
+// declaring function SCREAM
+// when it is called, it will execute the code
+// function scream(_numScreams, _name){
+//     for(let x = 0; x <_numScreams; x++){
+//         console.log(_name);
+//     }
+// }
+
+// calling function scream
+// parameters need to match the order as in function
+// scream(5, "Help");
+
